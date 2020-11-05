f9727dadbfc644576fada183106b7e23d37d90f1
rjackolantern <73896416+rjackolantern@users.noreply.github.com>
2020-11-04 23:24:18 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ from Player import Player
 # Initialize the list intended for storing Player objects
 players = []
 
+# Total players entered into tournament
 totalPlayers = 60
 
 # Close player list top view window
@@ -41,7 +42,7 @@ def exit_program():
 # Function passed to btnGetPlayers
 def get_players():
 
-    global players
+    global players, filename
 
     # Open file dialog, ask user to open a file
     # Open file dialog in the running directory of this program
@@ -52,7 +53,6 @@ def get_players():
         with open(filename, "r") as reader:
         
             line = reader.readline()
-        
             while line != "":
         
                 # Split the line into a list of split strings
@@ -63,13 +63,14 @@ def get_players():
         
                 # Append previously created player object to list
                 players.append(player)
-        
                 line = reader.readline()
     except FileNotFoundError:
-        pass
-
+        messagebox.showerror("Error", "This is not a valid file type")
+    except IndexError:
+        messagebox.showerror("Error", "This is not a valid file type")
     btnGenerate.config(state="normal")
     output_players()
+    
     
 # Output players to tree view top-level widget
 def output_players():
@@ -105,6 +106,7 @@ def sort_players(headingNum):
 
 # Delete player from tree view
 def delete_player():
+    global filename, players
     # Get user selection
     playerid = tview.selection()
     selectedPlayer = tview.item(playerid)["values"]
@@ -115,25 +117,56 @@ def delete_player():
         answer = messagebox.askyesno("Remove Item", "Are you sure you want to remove " + selectedPlayer[1] + " " + selectedPlayer[0]+ "?")
         # User confirms deletion
         if answer == True:
-            print("deleted")
-            tview.delete(playerid)
+            # Delete player object from list corresponding with tree view selection
+            for x in players:    
+                if (x.getLastName() + "," + x.getFirstName() + "," + str(x.getTierScore()) + ","  + x.getTier()) == (selectedPlayer[0] + "," + selectedPlayer[1] + "," + str(selectedPlayer[2]) + ","  + selectedPlayer[3]):
+                        players.remove(x)
+                        break
+        # Delete from tree view   
+        tview.delete(playerid)
+        # Update document
+        update_playerlist()
 
 # Add player to tree view            
 def add_player():
     global totalPlayers, players
+    addLastName, addFirstName = True, True
     # Can't have than 64 players
     if totalPlayers >= 64:
         messagebox.showwarning("Error", "No more players can be added.\nThe tournament has reached its capacity of 64 competitors")
-    else:
+    else: 
+        # Add first name
+        while addFirstName:
+            playerFirstName = simpledialog.askstring("Add Player", "Enter player's first name:")
+            # Player entered a string, go to next value
+            if playerFirstName != "":
+                addFirstName = False
+            else:
+                messagebox.showerror("Error", "Please add a first name")
         # Add last name
-        playerFirstName = simpledialog.askstring("Add Player", "Enter player's first name:")
-        # Add fist name
-        playerLastName = simpledialog.askstring("Add Player", "Enter player's last name:")
-        # Add rating, check if between 0 and 100
-        playerRating = simpledialog.askinteger("Add Player", "Enter player's rating (1-100):")
-        if playerRating > 100 or playerRating < 0: 
-            messagebox.showerror("Error", "Please enter a score between 0 and 100:")
-        else:
+        while addLastName:  
+            playerLastName = simpledialog.askstring("Add Player", "Enter player's last name:")    
+            # Player entered a string, go to next value
+            if playerLastName != "":
+                addLastName = False 
+            else:
+                messagebox.showerror("Error", "Please add a last name") 
+        # Check for duplicate player
+        duplicate = False
+        for i in tview.get_children():
+            x = tview.set(i)
+            # Last name and first names are the same
+            if x["1"] == playerLastName and x["2"] == playerFirstName:
+                messagebox.showerror("Error", playerFirstName + " " + playerLastName + " already exists!")
+                # Select player
+                tview.selection_set(i)
+                tview.see(i)
+                duplicate = True
+                break
+        # No duplicates found
+        if duplicate == False:
+            # Add rating, check if between 0 and 100
+            playerRating = simpledialog.askinteger("Add Player", "Enter player's rating (1-100):", minvalue=0, maxvalue=100)        
             # Calculate rank
             playerRank = calc_rank(playerRating) 
             # Insert new player
@@ -142,7 +175,76 @@ def add_player():
             tview.selection_set(playerid)
             # Scroll to new player
             tview.see(playerid)
- 
+            # Get value info on new player
+            info = tview.item(playerid)["values"]
+            # Create new player object with info
+            player = Player(info[0], info[1], info[2], info[3])
+            # Add player object to list
+            players.append(player)
+            # Update document
+            update_playerlist()
+            
+# Update document of players            
+def update_playerlist():
+    global players, filename, totalPlayers
+    index = 0
+    # Total players in tournament is total players in tree view
+    totalPlayers = len(tview.get_children())
+    for x in tview.get_children():
+        # Get values of item
+        info = tview.item(x)["values"]
+        # Set values of player object
+        players[index].setLastName(info[0])
+        players[index].setFirstName(info[1])
+        players[index].setTierScore(info[2])
+        players[index].setTier(info[3])
+        # Next object in list
+        index += 1
+    # Over write file  
+    with open(filename, "wt") as writer:
+        # For each player object, write comma delimited string on new line
+        for x in players:
+            writer.write(x.getLastName() + "," + x.getFirstName() + "," + str(x.getTierScore()) + "," + x.getTier() +"\n")
+            
+# Edit player rating
+def edit_player():
+    global filename
+    # Get user selection
+    playerid = tview.selection()
+    selectedPlayer = tview.item(playerid)["values"]
+    # Nothing selected
+    if selectedPlayer == "":
+        messagebox.showerror("Error", "Please select a player to edit.")
+    else:
+        # Edit rating, check if between 0 and 100
+        playerRating = simpledialog.askinteger("Add Player", "Enter player's rating (1-100):", minvalue=0, maxvalue=100) 
+        playerTier = calc_rank(playerRating) 
+        tview.set(playerid,column=3, value=playerRating)
+        tview.set(playerid,column=4, value=playerTier)
+        # Select new player
+        tview.selection_set(playerid)
+        # Scroll to new player
+        tview.see(playerid)  
+        # Update document  
+        update_playerlist()   
+# Search for player
+def search_player():
+    searchPlayer = simpledialog.askstring("Search Player", "Enter player's name (FistName LastName):")
+    # Search for player
+    found = False
+    for i in tview.get_children():
+        x = tview.set(i)
+        # Ignore case sensitivity when comparing names
+        if (x["2"] +  " " +  x["1"]).lower() == searchPlayer.lower():
+            # Select player
+            tview.selection_set(i)
+            tview.see(i)
+            found = True
+            break  
+    # Can't find player
+    if found == False:
+        messagebox.showerror("Search Player", searchPlayer + " is not entered in the tournament")
+               
 # Calculate player's rank based on their rating       
 def calc_rank(rating):
     if rating < 50:
@@ -155,27 +257,27 @@ def calc_rank(rating):
         return "Epic"
     elif rating >= 80:
         return "Legend"
+    # Doesn't correspond with existing ranks
     else:
         return None
-        
     
+# Initialize window and centre  
 root = Tk()
 root.title('Fortnite Team Tournament')
 root.geometry('%dx%d+%d+%d' % (912, 740, root.winfo_screenwidth() // 2 - 912 // 2,
     root.winfo_screenheight() // 2 - 740 // 2))
 root.resizable(False, False)
-
+# Create frame
 frame = Frame(root, padx=10, pady=10, bg='white')
 frame.pack()
-
+# Create Fornite banner 
 imgBanner = PhotoImage(file='images/fortnite_banner.png')
 lblBanner = Label(frame, image=imgBanner, padx=10, pady=10, borderwidth=0)
 lblBanner.grid(row=0, column=0, columnspan=5, pady=5)
-
+# Create team grid
 lblFrames = [0] * 16
 txtTeams = [0] * 16
 rownum, colnum = 1, 0
-
 for i in range(len(lblFrames)):
     lblFrames[i] = LabelFrame(frame, text='TEAM ' + str(i+1), bg='white', font=('Consolas', 11, 'bold'))
     txtTeams[i] = Text(lblFrames[i], width=25, height=6, font=('Consolas', 8), state='disabled', relief='flat', bg='white')
@@ -187,10 +289,10 @@ for i in range(len(lblFrames)):
         colnum = 0
     else:
         colnum += 1
-
+# Create frame for buttons
 buttonFrame = Frame(frame, padx=10, pady=10, bg='white')
 buttonFrame.grid(row=1, column=4, rowspan=4)
-
+# Create Buttons and pack to button frame
 btnPlayers = Button(buttonFrame, text='GET PLAYERS', width=15, height=2, command=get_players)
 btnPlayers.pack(side='top', padx=5, pady=5)
 btnView = Button(buttonFrame, text='VIEW PLAYERS', width=15, height=2, command=view_players)
@@ -201,13 +303,12 @@ btnSave = Button(buttonFrame, text='SAVE TEAMS', width=15, height=2, state='disa
 btnSave.pack(side='top', padx=5, pady=5)
 btnClear = Button(buttonFrame, text='CLEAR', width=15, height=2)
 btnClear.pack(side='top', padx=5, pady=5)
-
 btnExit = Button(buttonFrame, text='EXIT', width=15, height=2, command=exit_program)
 btnExit.pack(side='top', padx=5, pady=5)
-
+# Create Fortnite logo
 imgLogo = PhotoImage(file='images/fortnite_logo.png')
 lblLogo = Label(buttonFrame, image=imgLogo, borderwidth=0, bg='white').pack(side='top', padx=5, pady=5)
-
+# Create top-level widget for player list
 img = PhotoImage(file='images/fortnite.png')
 top_level = Toplevel(padx=10, pady=10, bg='white')
 top_level.title('Player List')
@@ -215,37 +316,33 @@ top_level.resizable(False, False)
 top_level.protocol('WM_DELETE_WINDOW', close_topview)
 top_level.geometry('%dx%d+%d+%d' % (490, 635, root.winfo_screenwidth() // 2 - 490 // 2, root.winfo_screenheight() // 2 - 635 // 2))
 top_level.withdraw()
- 
+
 lblImg = Label(top_level, image=img, bg='white')
 lblImg.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
-
+# Create tree view in top-level widget
 style = ttk.Style()
 style.configure('mystyle.Treeview.Heading', font=('Consolas', 11, 'bold'))
-
+# Tree view headings and columns
 tview = ttk.Treeview(top_level, selectmode='browse', columns=('1', '2', '3', '4'), show='headings', height=20, style='mystyle.Treeview')
 tview.grid(row=1, column=0, pady=5)
-
 headingtext = ('LAST NAME', 'FIRST NAME', 'RATING', 'TIER')
 columnwidths = [150, 150, 75, 75]
-
-# Tree view headings and columns
 for i in range(4):
     tview.column(str(i+1), width=columnwidths[i], anchor='w')
     tview.heading(str(i+1), text=headingtext[i], anchor='w', command=lambda columnid=i+1: sort_players(columnid))
-
+# Scroll bar
 vscroll = Scrollbar(top_level, orient='vertical', command=tview.yview)
 vscroll.grid(row=1, column=1, sticky='ns')
-
 bottomFrame = Frame(top_level, padx=5, pady=5, bg='white')
 bottomFrame.grid(row=2, column=0, columnspan=2)
-
+# Player list buttons
 btnRemove = Button(bottomFrame, text='REMOVE', width=10, pady=5, command=lambda: delete_player())
 btnRemove.pack(side='left', padx=5, pady=5)
 btnAdd = Button(bottomFrame, text='ADD', width=10, pady=5, command= lambda: add_player())
 btnAdd.pack(side='left', padx=5, pady=5)
-btnEdit = Button(bottomFrame, text='EDIT', width=10, pady=5)
+btnEdit = Button(bottomFrame, text='EDIT', width=10, pady=5, command= lambda: edit_player())
 btnEdit.pack(side='left', padx=5, pady=5)
-btnSearch = Button(bottomFrame, text='SEARCH', width=10, pady=5)
+btnSearch = Button(bottomFrame, text='SEARCH', width=10, pady=5, command = lambda: search_player())
 btnSearch.pack(side='left', padx=5, pady=5)
-
+# Output window
 root.mainloop()
